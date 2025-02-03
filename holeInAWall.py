34056e51a2bb791da16d9d6edef21ee70174f797
Gabri-Bore <gabriele.borello@itiscuneo.eu>
2025-02-03 12:29:35 +0100

    gu

--- holeInAWall.py
+++ holeInAWall.py
@@ -379,7 +379,6 @@ while game_running:
             time_threshold = max(10, time_threshold - 0.5)
 
     # Update the display
-    pygame.display.flip()
     pygame.display.update()
     clock.tick(60)
 
